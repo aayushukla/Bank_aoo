8e007f209360b4cac6e4948638521254f88b5096
shukla <aayush.shukla@capgemini.com>
2019-03-04 15:17:08 +0530

    Revert "Edited calculator for the second time.java"
    This reverts commit a06ad50e32da2a59fe19217032c247989007ae50.

--- CALCULATOR.java
+++ CALCULATOR.java
@@ -3,9 +3,5 @@ public class CALCULATOR
 public static void main(String [] args)
 	{
 System.out.println("HI");
-<<<<<<< HEAD
 	}
-=======
-System.out.println("How you doin?");	}
->>>>>>> parent of eec0e90... Modified calculator.java
 }
