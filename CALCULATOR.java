eec0e909343fc528f7e8deb28930ca1e343201d6
shukla <aayush.shukla@capgemini.com>
2019-03-04 14:36:13 +0530

    Modified calculator.java

--- CALCULATOR.java
+++ CALCULATOR.java
@@ -3,5 +3,11 @@ public class CALCULATOR
 public static void main(String [] args)
 	{
 System.out.println("HI");
-System.out.println("How you doin?");	}
+	}
+
+
+public static void add(int num1, int num2)
+{
+System.out.println("Addition="+num1+num2);
+}
 }
