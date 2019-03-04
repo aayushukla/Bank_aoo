a06ad50e32da2a59fe19217032c247989007ae50
shukla <aayush.shukla@capgemini.com>
2019-03-04 15:16:22 +0530

    Edited calculator for the second time.java

--- CALCULATOR.java
+++ CALCULATOR.java
@@ -3,5 +3,9 @@ public class CALCULATOR
 public static void main(String [] args)
 	{
 System.out.println("HI");
+<<<<<<< HEAD
 	}
+=======
+System.out.println("How you doin?");	}
+>>>>>>> parent of eec0e90... Modified calculator.java
 }
