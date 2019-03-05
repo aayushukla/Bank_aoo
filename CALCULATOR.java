8ab4d8e34bd71d91658e64a6aa0d1383e0d9e784
shukla <aayush.shukla@capgemini.com>
2019-03-05 10:50:08 +0530

    added division function()

--- CALCULATOR.java
+++ CALCULATOR.java
@@ -8,4 +8,8 @@ public static void addition(int number1,int number2)
 {
 System.out.println("Addition+"+(number1+number2));
 }
+public static void division(int number1,int number2) 
+{
+System.out.println("Addition+"+(number1/number2));
+}
 }
