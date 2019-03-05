514e2c173bfb4e00a817ffb3329d6b3e25b6e950
shukla <aayush.shukla@capgemini.com>
2019-03-05 10:43:11 +0530

    added subtraction function()

--- CALCULATOR.java
+++ CALCULATOR.java
@@ -8,4 +8,8 @@ public static void addition(int number1,int number2)
 {
 System.out.println("Addition+"+(number1+number2));
 }
+public static void subtraction(int number1,int number2) 
+{
+System.out.println("Addition+"+(number1-number2));
+}
 }
